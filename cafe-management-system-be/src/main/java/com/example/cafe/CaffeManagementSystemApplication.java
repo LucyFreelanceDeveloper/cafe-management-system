641203465bfad75cf9bd641d0f70ec2c2482a958
LucyQuantAnalyst <lucka.tester@outlook.com>
2023-10-12 16:21:53 +0200

    Implement Signup API of User: part 2 clean code

--- cafe-management-system-be/src/main/java/com/example/cafe/CaffeManagementSystemApplication.java
+++ cafe-management-system-be/src/main/java/com/example/cafe/CaffeManagementSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CaffeManagementSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CaffeManagementSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CaffeManagementSystemApplication.class, args);
+    }
 
 }
