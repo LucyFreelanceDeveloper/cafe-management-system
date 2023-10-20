aeaff9f39365e91ed449ea21571b29be692d2f7d
LucyQuantAnalyst <lucka.tester@outlook.com>
2023-10-20 14:45:27 +0200

    Correction in Api of Bill and Implemented Dashboard API

--- cafe-management-system-be/src/main/java/com/example/cafe/model/dto/BillDto.java
+++ cafe-management-system-be/src/main/java/com/example/cafe/model/dto/BillDto.java
@@ -1,9 +1,13 @@
 package com.example.cafe.model.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BillDto {
     private Integer id;
 
