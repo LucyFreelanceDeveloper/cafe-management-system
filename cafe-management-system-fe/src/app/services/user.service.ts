813531060074a3c368d42f765ae4fee3f382351a
LucyQuantAnalyst <lucka.tester@outlook.com>
2023-10-26 14:44:44 +0200

    Implement login part1

--- cafe-management-system-fe/src/app/services/user.service.ts
+++ cafe-management-system-fe/src/app/services/user.service.ts
@@ -21,4 +21,10 @@ export class UserService {
         headers:new HttpHeaders().set('Content-type', 'application/json')
     })
   }
+
+  login(data:any){
+    return this.httpClient.post(this.url + "/user/login", data, {
+        headers:new HttpHeaders().set('Content-type', 'application/json')
+    })
+  }
 }
