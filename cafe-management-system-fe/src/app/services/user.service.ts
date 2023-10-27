059460663943b08c07b08d9a8937657e034bfe72
LucyQuantAnalyst <lucka.tester@outlook.com>
2023-10-27 13:05:48 +0200

    Implement dashboard

--- cafe-management-system-fe/src/app/services/user.service.ts
+++ cafe-management-system-fe/src/app/services/user.service.ts
@@ -27,4 +27,8 @@ export class UserService {
         headers:new HttpHeaders().set('Content-type', 'application/json')
     })
   }
+
+  checkToken(){
+    return this.httpClient.get(this.url + "/user/checkToken");
+  }
 }
