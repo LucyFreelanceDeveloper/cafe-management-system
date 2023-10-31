2a4facd785655cd1f53b8801dc1572aa9464e061
LucyQuantAnalyst <lucka.tester@outlook.com>
2023-10-31 15:47:08 +0100

    correction in config file:
    - add import java.util.List;

--- cafe-management-system-be/src/main/java/com/example/cafe/config/security/SecurityConfig.java
+++ cafe-management-system-be/src/main/java/com/example/cafe/config/security/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
+import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
