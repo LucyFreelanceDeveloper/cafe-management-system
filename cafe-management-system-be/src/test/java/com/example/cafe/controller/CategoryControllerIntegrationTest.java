e8d3648f74ecec52dfca463d73ba6623854caa9f
LucyFreelanceDeveloper <lucka.tester@outlook.com>
2024-02-23 17:11:18 +0100

    cafe-management-system-be: create tests for CategoryControllerIntegrationTest

---
+++ cafe-management-system-be/src/test/java/com/example/cafe/controller/CategoryControllerIntegrationTest.java
@@ -0,0 +1,87 @@
+package com.example.cafe.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CategoryControllerIntegrationTest {
+
+    public static final String ADMIN_MAIL = "admin@mailnator.com";
+    public static final String ADMIN_PASSWORD = "12345";
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DirtiesContext
+    void create() throws Exception {
+        Map<String, Object> createRequest = new HashMap<>();
+        createRequest.put("name", "Pizza1");
+
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(createRequest, loginAdminUserHeaders());
+
+        ResponseEntity<String> response = restTemplate.postForEntity("/categories", entity, String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    }
+
+    @Test
+    void findAll() throws Exception {
+        HttpEntity<String> entity = new HttpEntity<>("application/json", loginAdminUserHeaders());
+
+        ResponseEntity<String> response = restTemplate.exchange("/categories", HttpMethod.GET, entity, String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    @DirtiesContext
+    void update() throws Exception {
+        Map<String, Object> createRequest = new HashMap<>();
+        createRequest.put("categoryId", "1");
+        createRequest.put("name", "Pizza1");
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(createRequest, loginAdminUserHeaders());
+
+        ResponseEntity<String> response = restTemplate.postForEntity("/categories", entity, String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    }
+
+    @Test
+    @DirtiesContext
+    void delete() throws Exception {
+        HttpEntity<String> entity = new HttpEntity<>("application/json", loginAdminUserHeaders());
+
+        ResponseEntity<String> response = restTemplate.exchange("/categories/1", HttpMethod.DELETE, entity, String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    private HttpHeaders loginAdminUserHeaders() throws Exception {
+        Map<String, String> loginRequest = Map.of("email", ADMIN_MAIL,
+                "password", ADMIN_PASSWORD);
+        ResponseEntity<String> loginResponse = restTemplate.postForEntity("/user/login", loginRequest, String.class);
+        assertThat(loginResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer " + obtainAccessToken(ADMIN_MAIL, ADMIN_PASSWORD));
+        return headers;
+    }
+
+    private String obtainAccessToken(String username, String password) throws Exception {
+        Map<String, String> params = Map.of("email", username, "password", password);
+        ResponseEntity response = restTemplate.postForEntity("/user/login", params, String.class);
+        assertThat(response.getStatusCode().toString()).isEqualTo(HttpStatus.OK.toString());
+        return objectMapper.readValue(response.getBody().toString(), Map.class).get("token").toString();
+    }
+}
