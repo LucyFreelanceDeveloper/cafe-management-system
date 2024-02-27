380d3a7809cf95ed9ac12bbe4a96e6e59b18a406
LucyFreelanceDeveloper <lucka.tester@outlook.com>
2024-02-27 16:27:45 +0100

    cafe-management-system-be: add mock email

--- cafe-management-system-be/src/test/java/com/example/cafe/config/Config.java
+++ cafe-management-system-be/src/test/java/com/example/cafe/config/Config.java
@@ -1,10 +1,13 @@
 package com.example.cafe.config;
 
+import jakarta.mail.Session;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @Configuration
 public class Config {
@@ -12,7 +15,7 @@ public class Config {
     @Bean
     public JavaMailSender javaMailSender(){
         JavaMailSender javaMailSender = mock(JavaMailSender.class);
-
+        when(javaMailSender.createMimeMessage()).thenReturn(new MimeMessage((Session) null));
         return javaMailSender;
     }
 }
